729180733b615cf7dfc16181f607df15a0e6828b
YonaMilstein <yonahalimi@gmail.com>
2019-02-15 14:22:22 +0200

    login + user view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserViewComponent } from './user/user-view/user-view.component';
+
+// create routing
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'user', component: UserViewComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
