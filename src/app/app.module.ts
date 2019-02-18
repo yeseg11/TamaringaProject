015c6a73dbe4256386bacb0ed3ca572ed1d18745
milstein <david.milstein@microfocus.com>
2019-02-18 18:36:32 +0200

    Create a Server + app.js (backend) Create new screen of new user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { Validators} from '@angular/forms';
 
+// import of Angular Material Components that used for project
+import { MatInputModule,
+         MatCardModule,
+         MatButtonModule } from '@angular/material';
+
+// Routing
 import { AppRoutingModule } from './app-routing.module';
 
+// Components in project
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UserViewComponent } from './user/user-view/user-view.component';
+import { AdminViewComponent } from './admin/admin-view/admin-view.component';
+import { NewUserComponent } from './admin/new-user/new-user.component';
 
 // create routing
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'user', component: UserViewComponent }
+  { path: 'user', component: UserViewComponent },
+  { path: 'admin', component: AdminViewComponent },
+  { path: 'admin/new-user', component: NewUserComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    UserViewComponent
+    UserViewComponent,
+    AdminViewComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
